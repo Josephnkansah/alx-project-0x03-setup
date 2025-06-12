7e0a037930a578845cf00d3b5e403ace79ad9d98
JosephNkansah <jnkansahoseph4@gmail.com>
2025-06-12 09:15:34 +0000

    5

---
+++ alx-project-0x05/pages/text-to-image.tsx
@@ -0,0 +1,9 @@
+const TextToImage = () => {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h2 className="text-3xl font-semibold">Text to Image Page</h2>
+      </div>
+    );
+  };
+  
+  export default TextToImage;
